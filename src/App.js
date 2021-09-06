17df4a2d62b410c5dbb46807caf7adac98835a92
Samuel Heluany <samuelheluany@hotmail.com>
2021-09-06 11:19:54 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import md5 from 'md5'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [marvel, setMarvel] = useState([])
+
+  const baseURL= 'http://gateway.marvel.com/v1/public/characters?'
+  
+  const publicKey ='cd6f735e64cb3d6f709d69cb1170653d'
+  const privateKey ='377215d3ad30a3838862bcdd13cdc40db41e98da'
+  
+  const time = Number(new Date())
+  const hash = md5(time + privateKey + publicKey)
+
+  const marvelApi = async () => {
+    const result = await axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`)
+    setMarvel(result.data.data.results)
+    console.log(result.data.data.results)
+  }
+  
+  useEffect(() => {
+    marvelApi()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="marvelLogo">
+          <h1>Marvel API</h1>
+      </div>
+    <div className="marvelCharacters" >
+      {marvel.map((details) => {
+        return (
+          <div key={details.id} className="charactersDetails">
+            <img src={`${details.thumbnail.path}.${details.thumbnail.extension}`} alt="Marvel Image Character" />
+            <h3>{details.name}</h3>
+          </div>
+        )
+      })}
+    </div>
     </div>
   );
 }
